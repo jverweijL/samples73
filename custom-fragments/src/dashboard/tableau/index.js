5018f0e2fa8ca62438fc4fd21f4a6ef359082ccb
Jan Verweij <jan.verweij@liferay.com>
2020-11-23 09:26:33 +0100

    fixing tableau

--- custom-fragments/src/dashboard/tableau/index.js
+++ custom-fragments/src/dashboard/tableau/index.js
@@ -1,7 +1,16 @@
-var editing = !!document.getElementsByClassName('fragments-editor').length;
+var editing = !!document.getElementsByClassName('page-editor').length;
+var element = fragmentElement.getElementsByClassName("edit-frag")[0];
 
 if (editing) {
-  $(fragmentElement.getElementsByClassName("edit")).removeClass("invisible");
+  element.classList.remove("invisible");
 } else {
-  $(fragmentElement.getElementsByClassName("edit")).addClass("invisible");
+  element.classList.add("invisible");
 }
+
+var divElement = fragmentElement.getElementsByClassName("tableauPlaceholder")[0];
+var vizElement = divElement.getElementsByTagName('object')[0]; 
+vizElement.style.width = '100%'; 
+vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; 
+var scriptElement = document.createElement('script'); 
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; 
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
