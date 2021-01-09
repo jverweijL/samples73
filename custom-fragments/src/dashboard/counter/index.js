5940d755e34d855f5a70a517eca6cb3616472316
Jan Verweij <jan.verweij@liferay.com>
2021-01-09 10:13:53 +0100

    dropzone & counter added

---
+++ custom-fragments/src/dashboard/counter/index.js
@@ -0,0 +1,15 @@
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = fragmentElement.getElementsByClassName("counter-fragment")[0];
+animateValue(obj, parseInt(configuration.from), parseInt(configuration.to), 5000);
