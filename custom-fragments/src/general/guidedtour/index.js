59c5d424066c809262139bf00309ee721d6b210e
Jan Verweij <jan.verweij@liferay.com>
2021-01-22 17:21:27 +0100

    guided tour

--- custom-fragments/src/general/guidedtour/index.js
+++ custom-fragments/src/general/guidedtour/index.js
@@ -1,8 +1,23 @@
-introJs().setOptions({
-  steps: [{
-    intro: "Hello world!"
-  }, {
-    element: document.querySelector('span.sign-in'),
-    intro: "Click here to login!"
-  }]
-}).start();
+var editing = !!document.getElementsByClassName('page-editor').length;
+var element = fragmentElement.getElementsByClassName("edit-frag")[0];
+
+if (editing) {
+    // do whatever is needed
+	element.classList.remove("invisible");
+} else {
+		element.classList.add("invisible");
+    if (configuration.runOnce) {
+        // already ran before
+    } else {
+        introJs().setOptions({
+            steps: [{
+                intro: "Hello world!"
+            }, {
+                element: document.querySelector('span.sign-in'),
+                intro: "Click here to login!"
+            },{
+                intro: "You are done"
+            }]
+        }).start();
+    }
+}
